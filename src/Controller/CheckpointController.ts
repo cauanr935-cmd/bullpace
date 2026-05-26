61fb2a3e56c5eb212cc7ab1a30576fe5e496e162
cauan <cauan.martins@sou.inteli.edu.br>
2026-05-26 11:23:20 -0300

    fix(#219): corrige node_modules

--- src/Controller/CheckpointController.ts
+++ src/Controller/CheckpointController.ts
@@ -1,7 +1,8 @@
-// Biblioteca do supabase que possibilita abstrair o processo de 
+// Biblioteca do supabase que possibilita abstrair o processo de inserir dados, atualizar, deletar e etc...
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://fhxjysgowbdrfdwlxurv.supabase.co';
+// Chave anion, basiamente o identificador do projeto, é necessário para autenticar as requisições feitas para o banco de dados do supabase
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoeGp5c2dvd2JkcmZkd2x4dXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0ODM4NDQsImV4cCI6MjA5NDA1OTg0NH0.KCemCejnePNuLTYcMDrmHEt3Aqs6ntqNamphhBtGcyM'; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
